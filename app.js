b33ebe37d04e16dca044f9071e38b5d498bd885d
James Newman <jamesmnewman1@gmail.com>
2015-07-01 01:13:23 -0700

    notes

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ angular.module('flapperNews', ['ui.router'])
 // services are a means of keeping data around for the lifetime of an application, and can be used across different controllers
 // the 3 ways to do that are a factory, service, or provider
 // factory; you create an object, add properties to it, then return that same object. when you pass the service into the controller, those properties on the object are available in that controller
-// 
+// service; instantiated w/ the new keyword. Doing that allows you to call 'this'. When passed into the controller, those properties will be available in that controller through the service
+// providers; can be passed into the .config function, which I believe is what is referenced above in our routes. Providers are used when you want to provide module like config for the service object, before being made available
 
 
 // setting up angular app called 'Flapper News'
