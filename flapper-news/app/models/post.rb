2db603fee602dfc1e8b1b90c28595162bc2e3894
James Newman <jamesmnewman1@gmail.com>
2015-07-05 18:13:46 -0700

    added finishing touches, and associating users with posts and comments. Final step to push to heroku

--- flapper-news/app/models/post.rb
+++ flapper-news/app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ActiveRecord::Base
 	has_many :comments
+	belongs_to :user
 
 	# this is taking the place of using the rabl gem. Rabl makes all of your model data json
 	# which allows you to get all of your backend DB data as json and do stuff with it.
 	# this is good for angular. We take rails json data, and manipulate it on the front end with angular.
 	def as_json(options = {})
-		super(options.merge(include: :comments))
+		super(options.merge(include: [:user, comments: {include: :user}]))
 	end
 end
